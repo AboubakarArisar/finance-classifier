b2f9a963676443ba83c0b6491ccf9c30255de1c4
AboubakarArisar <aboubakar.bscsf22@iba-suk.edu.pk>
2026-07-02 22:30:00 +0500

    added logo and public url

--- app/page.tsx
+++ app/page.tsx
@@ -313,7 +313,7 @@ export default function Home() {
             <h2 className="text-2xl font-semibold text-text-strong">העלאת קבצים</h2>
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
-              src="https://res.cloudinary.com/dnpxugbk9/image/upload/v1783012114/logo_tupgk3.jpg"
+              src="https://res.cloudinary.com/dnpxugbk9/image/upload/v1783013365/logo_lrsktq.png"
               alt="Benny Vazana"
               className="h-14 w-auto object-contain"
             />
